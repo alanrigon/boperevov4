4c93386c05f41fdf8c5475817fe29aeb15c91d5f
alanrigon <alanrigon845@gmail.com>
2025-07-03 14:35:36 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAAbc9dZu9l89pP7DBHE7eHIw1OaViLXxg",
-  authDomain: "v4-bope-revoada-rj.firebaseapp.com",
-  projectId: "v4-bope-revoada-rj",
-  storageBucket: "v4-bope-revoada-rj.firebasestorage.app",
-  messagingSenderId: "651254590252",
-  appId: "1:651254590252:web:c632fd2bd29ffc696c5a89"
+  apiKey: "AIzaSyA-N5DCcifhlvE2y9glGMJigT-Vq5JdKUo",
+  authDomain: "painel-v4bope.firebaseapp.com",
+  projectId: "painel-v4bope",
+  storageBucket: "painel-v4bope.firebasestorage.app",
+  messagingSenderId: "331256836362",
+  appId: "1:331256836362:web:2a910baf0ad8a9cd440af3",
 };
